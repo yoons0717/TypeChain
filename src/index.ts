8731165f55dc74c8412c4f0eb343b35e44b4b385
yoonmi <yoons0717@gmail.com>
2020-11-14 19:12:55 +0900

    Create Block

---
+++ src/index.ts
@@ -0,0 +1,47 @@
+import * as CryptoJS from 'crypto-js';
+
+class Block{
+    public index:number;
+    public hash:string;
+    public previousHash: string;
+    public data: string;
+    public timestamp: number;
+
+    
+// 함수를 static으로 선언하여서 Block을 생성하지 않아도 동작
+    static calculateBlockHash = (
+        index:number, 
+        previousHash:string, 
+        timestamp:number, 
+        data:string
+        ):string => 
+        CryptoJS.SHA256(index + previousHash + timestamp + data).toString();
+    
+
+
+    constructor(
+        index:number,
+        hash:string,
+        previousHash: string,
+        data: string,
+        timestamp: number
+    ){
+        this.index = index;
+        this.hash = hash;
+        this.previousHash = previousHash;
+        this.data = data;
+        this.timestamp = timestamp;
+    }
+}
+
+const genesisBlock:Block = new Block(0,"2020202020","","Hello",123456);
+
+let blockchain: Block[] = [genesisBlock];
+
+const getBlockchain = () : Block[] => blockchain;
+
+const getLatestBlock = () : Block=> blockchain[blockchain.length-1];
+
+const getNewTimeStamp = () : number => Math.round(new Date().getTime() / 1000);
+
+export {};
